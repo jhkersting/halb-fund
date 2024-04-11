0e989a790468cd6ac99c93bb421f06847a82fd91
jhkersting <56937882+jhkersting@users.noreply.github.com>
2024-04-11 17:40:58 -0500

    saf

--- final/portfolio-weights.py
+++ final/portfolio-weights.py
@@ -7,7 +7,17 @@ years = list(range(2001,2023))
 output = pd.DataFrame()
 for y in years:
     portfolio = pd.read_csv(str(y) + '/mkt-rf-alpha/portfolio.csv')
-    portfolio = portfolio[['year','ticker','weight_z-score','alpha','beta',]]
+    portfolio['weight'] = portfolio['weight_z-score']
+    portfolio = portfolio[['year','ticker','weight','alpha','beta',]]
     output = pd.concat([output,portfolio])
 
 output.round(4).to_csv('yearly-portfolio-weights.csv', index=False)
+
+
+# clean portfolio returns
+port = pd.read_csv('portfolio-full-output.csv')
+port['return'] = port['z-score']
+
+port = port[['date','return','mkt-rf']]
+
+port.round(4).to_csv('daily-portfolio-returns.csv', index=False)
